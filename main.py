1141330b3a5f6aaf2222624bf23b3d7d9c1e55ae
Finn McKinley <finnmckinley@proton.me>
2024-05-30 21:41:57 +1200

    Updated main.py
    Allowed for custom filenames using terminal input. Also fixed the code ignoring line breaks, so now it actually formats correctly!
    
    Also may run quicker? 2 for loops were removed which were redundant anyways.

--- main.py
+++ main.py
@@ -10,39 +10,60 @@ START = time.time()
 def read_file(filename):
     """ Reads a file given from filename and returns file content """
     file = open(filename)
-    lines = file.read().splitlines()
+    content = file.read()
     file.close()
-    return lines
+    return content
 
-def character_map(lines):
+def character_map(content):
     """ Takes every line from file and returns character map """
     characters = []
-    for line in lines:
-        for character in line:
-            if character not in characters:
-                characters.append(character)
+    for character in content:
+        if character not in characters:
+            characters.append(character)
     return characters
 
 def random_letter(character_map):
     """ Selects a random letter from the character map """
     return character_map[rdm.randint(0, len(character_map) -1)]
 
+def file_guesser(filename):
+    """ Randomly guesses its way through a file """
+    content = read_file(filename)
+    CHARACTERS = character_map(content)
+    solution = ''
+    for character in content:
+        guess = random_letter(CHARACTERS)
+        while guess != character:
+            guess = random_letter(CHARACTERS)
+        solution += character
+    return solution
+
+def get_filename():
+    """ Asks the user for the filename until it exists """
+    prompt = 'What is the filename you want to run? '
+    generic_error = 'Sorry that is not a valid filename!'
+    
+    is_valid = False
+    while is_valid == False:
+        user_in = input(prompt)
+        try: 
+            file = open(user_in)
+        except:
+            print(generic_error)
+        else:
+            print('')
+            is_valid = True
+    return user_in
+
 def main():
     """ Runs all of the code together
     """
-    lines = read_file('shakespeare.txt')
-    CHARACTERS = character_map(lines)
-    solution = ''
-    for line in lines:
-        for character in line:
-            guess = random_letter(CHARACTERS)
-            while guess != character:
-                guess = random_letter(CHARACTERS)
-            solution += character
-    print(solution)
+    filename = get_filename()
+    print(file_guesser(filename))
+
             
 main()
 
 # Tracks Program Runtime
 END = time.time()
-print(f'The time of execution of above program is : {(END - START) * 10**3}, ms')
+print(f'\n** The time of execution of above program is : {(END - START) * 10**3} ms **')
